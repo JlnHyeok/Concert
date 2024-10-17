de4a36dd60f9dfbf81c60f200851a3980c363af2
JinHyeok <gjwlsgur123@gmail.com>
2024-10-18 05:56:56 +0900

    Fix: User Service 동시성을 위한 락 적용

--- src/infra/repositories/balance.repository.ts
+++ src/infra/repositories/balance.repository.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Balance } from 'src/domain/user/model/entity/balance.entity';
-import { IBalanceRepository } from 'src/domain/user/model/repository/balance.repository';
-import { Repository } from 'typeorm';
+import { Balance } from '../../domain/user/model/entity/balance.entity';
+import { IBalanceRepository } from '../../domain/user/model/repository/balance.repository';
+import { DataSource, Repository, EntityManager } from 'typeorm';
 
 @Injectable()
 export class BalanceRepository
   extends Repository<Balance>
   implements IBalanceRepository
 {
+  constructor(private readonly dataSource: DataSource) {
+    super(Balance, dataSource.createEntityManager());
+  }
+
   async findByUserId(id: number): Promise<Balance> {
     return await this.findOne({
       where: { userId: id },
     });
   }
+
   async createBalance(balance: Balance): Promise<Balance> {
     return await this.save(balance);
   }
 
-  async updateBalance(updateBalance: Balance): Promise<Balance> {
-    return await this.save(updateBalance);
+  async updateBalance(
+    updateBalance: Balance,
+    manager: EntityManager,
+  ): Promise<Balance> {
+    return await manager.save(updateBalance); // Use manager for transaction context
   }
 }
