85f5e7d5671361af6a9f73e774612e50e8dca060
JinHyeok <gjwlsgur123@gmail.com>
2024-10-18 03:59:17 +0900

    패키지 설치 및 불필요한 파일 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './infra/database/database.module';
@@ -20,17 +19,36 @@ import { PAYMENT_REPOSITORY } from './domain/reservation/model/repository/paymen
 import { PaymentRepository } from './infra/repositories/payment.repository';
 import { WaitingQueueService } from './domain/waiting-queue/services/waiting-queue.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { UserService } from './domain/user/services/user.service';
+import { ConcertService } from './domain/concert/service/consert.service';
+import { ReservationService } from './domain/reservation/service/reservation.service';
+import { ReservationFacade } from './application/facades/reservation.facade';
+import { SEAT_REPOSITORY } from './domain/concert/model/repository/seat.repository';
+import { SeatRepository } from './infra/repositories/seat.repository';
+import { UserController } from './presentation/controller/user.controller';
+import { ConcertController } from './presentation/controller/concert.controller';
+import { ReservationController } from './presentation/controller/reservation.controller';
 
 @Module({
   imports: [DatabaseModule, ScheduleModule.forRoot()],
-  controllers: [AppController, WaitingQueueController],
+  controllers: [
+    WaitingQueueController,
+    UserController,
+    ConcertController,
+    ReservationController,
+  ],
   providers: [
     AppService,
     WaitingQueueService,
+    UserService,
+    ConcertService,
+    ReservationService,
+    ReservationFacade,
     { provide: USER_REPOSITORY, useClass: UserRepository },
     { provide: BALANCE_REPOSITORY, useClass: BalanceRepository },
     { provide: WAITING_QUEUE_REPOSITORY, useClass: WaitingQueueRepository },
     { provide: RESERVATION_REPOSITORY, useClass: ReservationRepository },
+    { provide: SEAT_REPOSITORY, useClass: SeatRepository },
     { provide: PAYMENT_REPOSITORY, useClass: PaymentRepository },
     { provide: CONCERT_REPOSITORY, useClass: ConcertRepository },
     {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Concert 예약 시스템')
+    .setDescription('Concert 예약 시스템 API 문서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
