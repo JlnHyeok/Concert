a342db347d0672efd6aea9e99663ec3b22f1cc65
JinHyeok <gjwlsgur123@gmail.com>
2024-10-25 05:49:47 +0900

    DTO 및 Entity 수정

--- src/presentation/dto/request/concert.request.dto.ts
+++ src/presentation/dto/request/concert.request.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class GetScheduleRequestDto {
   @ApiProperty({ description: '공연 ID' })
+  @Transform(({ value }) => Number(value))
   @IsNumber()
   @IsNotEmpty()
   concertId: number;
@@ -10,11 +12,13 @@ export class GetScheduleRequestDto {
 
 export class GetSeatsRequestDto {
   @ApiProperty({ description: '공연 ID' })
+  @Transform(({ value }) => Number(value))
   @IsNumber()
   @IsNotEmpty()
   concertId: number;
 
   @ApiProperty({ description: '공연 날짜' })
+  @Transform(({ value }) => Number(value))
   @IsNotEmpty()
   @IsDate()
   performanceDate: string;
--- src/presentation/dto/response/user.response.dto.ts
+++ src/presentation/dto/response/user.response.dto.ts
@@ -1,8 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateUserResponseDto {
+  @ApiProperty({ description: '유저 ID' })
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  @IsNotEmpty()
+  id: number;
+
+  @ApiProperty({ description: '유저 이름' })
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({ description: '포인트' })
+  @Transform(({ value }) => Number(value))
+  @IsNumber()
+  balance: number;
+}
 
 export class ChargePointResponseDto {
   @ApiProperty({ description: '포인트' })
+  @Transform(({ value }) => Number(value))
   @IsNumber()
   @IsNotEmpty()
   balance: number;
@@ -10,6 +29,7 @@ export class ChargePointResponseDto {
 
 export class CheckPointResponseDto {
   @ApiProperty({ description: '포인트' })
+  @Transform(({ value }) => Number(value))
   @IsNumber()
   @IsNotEmpty()
   balance: number;
