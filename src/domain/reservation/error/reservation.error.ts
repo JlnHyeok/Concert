e251f42a251d8b39bbbea970dd57edb375207e68
JinHyeok <gjwlsgur123@gmail.com>
2024-10-25 05:43:16 +0900

    Business Exception 및 커스텀 에러 작성

---
+++ src/domain/reservation/error/reservation.error.ts
@@ -0,0 +1,10 @@
+export const RESERVATION_ERROR_CODES = {
+  RESERVATION_NOT_FOUND: {
+    code: 'RESERVATION_001',
+    message: 'Reservation not found',
+  },
+  PRICE_INVALID: {
+    code: 'RESERVATION_002',
+    message: 'Price is invalid',
+  },
+};
---
+++ src/domain/waiting-queue/error/waiting-queue.error.ts
@@ -0,0 +1,16 @@
+export const WAITING_QUEUE_ERROR_CODES = {
+  TOKEN_EXPIRED: {
+    code: 'WAITING_QUEUE_002',
+    message: 'Token expired',
+  },
+
+  TOKEN_INVALID: {
+    code: 'WAITING_QUEUE_003',
+    message: 'Token is invalid',
+  },
+
+  QUEUE_INFO_NOT_FOUND: {
+    code: 'WAITING_QUEUE_004',
+    message: 'Queue information not found',
+  },
+};
