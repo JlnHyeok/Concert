1428f03c66e7b00a62dafbaf74c73397207825d4
Heo Jin Hyeok <gjwlsgur123@gmail.com>
2024-11-18 19:08:32 +0900

    Payment Server kafka 적용

--- docker/payment_server/src/server.js
+++ docker/payment_server/src/server.js
@@ -1,10 +1,43 @@
 // server.js
+const { Kafka } = require('kafkajs');
 const express = require('express');
 
 const app = express();
 const port = 4000;
 
+const kafka = new Kafka({
+  clientId: 'payment_server',
+  brokers: ['127.0.0.1:10000'],
+});
+
+const consumer = kafka.consumer({ groupId: 'payment-group' });
+const producer = kafka.producer();
+
 // 외부 API 설정
+const initKafka = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'payment' });
+
+  await producer.connect();
+  await consumer?.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log('외부 API 호출');
+      console.log({
+        value: message.value.toString(),
+      });
+      await producer.send({
+        topic: 'payment.reply',
+        messages: [
+          {
+            value: JSON.stringify({ isSuccess: true, message: '결제 성공' }),
+          },
+        ],
+      });
+      console.log('결제 완료');
+    },
+    autoCommit: true,
+  });
+};
 
 // 특정 엔드포인트 호출 예시
 app.post('/api/payment', async (req, res) => {
@@ -16,6 +49,8 @@ app.post('/api/payment', async (req, res) => {
     res.status(500).json({ message: '결제 실패', error: error.message });
   }
 });
+
+initKafka();
 
 // 서버 실행
 app.listen(port, () => {
