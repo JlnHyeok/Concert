02cfc3a23ac4c913ec8315546fdf322ec8ac04c9
JinHyeok <gjwlsgur123@gmail.com>
2024-11-20 01:43:34 +0900

    Fix: Payment Api Outbox Pattern 및 보상 트랜잭션 & 발행 실패 스케줄러 작성

--- src/application/facades/reservation/reservation.facade.ts
+++ src/application/facades/reservation/reservation.facade.ts
@@ -4,14 +4,17 @@ import { UserService } from '../../../domain/user/services/user.service';
 import { ConcertService } from '../../../domain/concert/service/consert.service';
 import { Injectable, Inject } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
-import { DataSource } from 'typeorm';
 import { Seat } from '../../../domain/concert/model/entity/seat.entity';
 import { Reservation } from '../../../domain/reservation/model/entity/reservation.entity';
 import { Payment } from '../../../domain/reservation/model/entity/payment.entity';
-import { ClientKafka, MessagePattern } from '@nestjs/microservices';
-import { EachMessagePayload } from 'kafkajs';
+import { ClientKafka } from '@nestjs/microservices';
 import { timeout } from 'rxjs';
-import { PaymentCreatedEventStatus } from 'src/domain/reservation/model/entity/payment.created.event.entity';
+import {
+  IPaymentOutboxMetadata,
+  PaymentOutboxStatus,
+} from '../../../domain/reservation/model/entity/payment.outbox.entity';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class ReservationFacade {
@@ -26,7 +29,8 @@ export class ReservationFacade {
     private readonly reservationService: ReservationService,
     @Inject('KAFKA_CLIENT')
     private readonly kafkaClient: ClientKafka,
-    private readonly dataSource: DataSource,
+    @InjectEntityManager()
+    private readonly entityManager: EntityManager,
   ) {}
 
   async createReservation({
@@ -53,22 +57,25 @@ export class ReservationFacade {
     //#endregion
   }
 
-  async createPayment(token: string, userId: number, seatId: number) {
+  async createPayment(
+    token: string,
+    userId: number,
+    seatId: number,
+  ): Promise<void> {
     let seat: Seat;
     let reservation: Reservation;
-    let payment: Payment;
 
     console.log('createPayment Execute');
-    await this.dataSource.transaction(async (manager) => {
+    await this.entityManager.transaction(async (manager) => {
       // 1. 좌석 상태 확인
       seat = await this.concertService.checkSeatStatusBySeatId(
         seatId,
         'HOLD',
         manager,
       );
-
-      // 2. 유저 포인트 사용
-      await this.userService.usePoint(userId, seat.price, manager);
+      // 2. 좌석 상태 변경
+      seat.status = 'RESERVED';
+      await this.concertService.updateSeat(seat.id, seat, manager);
 
       // 3. 예약 확인
       reservation =
@@ -78,12 +85,11 @@ export class ReservationFacade {
           manager,
         );
 
-      // 4. 좌석 상태 변경
-      seat.status = 'RESERVED';
-      await this.concertService.updateSeat(seat.id, seat, manager);
+      // 4. 유저 포인트 사용
+      await this.userService.usePoint(userId, seat.price, manager);
 
       // SAVE META DATA
-      const metaData = {
+      const metadata: IPaymentOutboxMetadata = {
         userId: userId,
         token: token,
         price: seat.price,
@@ -91,13 +97,12 @@ export class ReservationFacade {
         seatId: seat.id,
       };
 
-      // 5. 결제 이벤트 Outbox 생성
-      console.log('createPaymentCreatedEvent Execute');
-      const paymentCreatedEvent =
-        await this.reservationService.createPaymentCreatedEvent(
-          metaData,
-          manager,
-        );
+      // 5. 결제 이벤트 Outbox Data 저장
+      console.log('createPaymentOutbox Execute');
+      const paymentOutbox = await this.reservationService.createPaymentOutbox(
+        metadata,
+        manager,
+      );
 
       console.log('emit payment event');
 
@@ -106,43 +111,51 @@ export class ReservationFacade {
         .emit('payment', {
           key: `payment_reservation_${reservation.id}`,
           value: {
-            eventId: paymentCreatedEvent.id,
-            metaData,
+            eventId: paymentOutbox.id,
+            metadata,
           },
         })
         .pipe(timeout(5000));
     });
 
-    return payment;
+    return;
   }
 
-  async InvokePaymentReply(id: number, status: PaymentCreatedEventStatus) {
-    await this.reservationService.updatePaymentCreatedEventStatus(id, status);
+  async InvokePaymentReply(id: number, status: PaymentOutboxStatus) {
+    await this.reservationService.updatePaymentOutboxStatus(id, status);
   }
 
   async InvokePaymentSucess(props: {
     eventId: number;
     token: string;
-    status: PaymentCreatedEventStatus;
+    status: PaymentOutboxStatus;
   }) {
     const { eventId, token, status } = props;
-    await this.reservationService.updatePaymentCreatedEventStatus(
-      eventId,
-      status,
-    );
+    await this.reservationService.updatePaymentOutboxStatus(eventId, status);
     this.waitingQueueService.expireToken(token);
+    // + alarm,,, slack,,,
   }
 
+  // 보상 트랜잭션
   async InvokePaymentFail(props: {
     eventId: number;
-    userId: number;
-    price: number;
+    metadata: IPaymentOutboxMetadata;
   }) {
-    const { eventId, userId, price } = props;
+    const { eventId, metadata } = props;
+    const { userId, price, seatId } = metadata;
+
+    // 1. 유저 포인트 복구
     await this.userService.chargePoint(userId, price);
-    await this.reservationService.updatePaymentCreatedEventStatus(
+
+    // 2. 좌석 상태 변경
+    let seat = await this.concertService.getSeat(seatId);
+    seat.status = 'AVAILABLE';
+    await this.concertService.updateSeat(seatId, seat);
+
+    // 3. 결제 이벤트 상태 변경
+    await this.reservationService.updatePaymentOutboxStatus(
       eventId,
-      PaymentCreatedEventStatus.FAIL,
+      PaymentOutboxStatus.FAIL,
     );
   }
 
@@ -158,17 +171,34 @@ export class ReservationFacade {
       if (new Date(seat.releaseAt) <= now) {
         seat.status = 'AVAILABLE';
         await this.concertService.updateSeat(seat.id, seat);
-
         await this.reservationService.deleteReservationBySeatId(seat.id);
       }
     }
   }
 
-  // private async checkActivatedToken(token: string) {
-  //   const tokenInfo = await this.waitingQueueService.checkWaitingQueue(token);
-  //   if (!tokenInfo?.expireAt) throw new BadRequestException('Invalid token');
-  //   if (new Date(tokenInfo.expireAt).getTime() > new Date().getTime()) {
-  //     throw new BadRequestException('Token expired');
-  //   }
-  // }
+  @Cron('0 */3 * * * *')
+  async retryFailedCreatePayment() {
+    const failedEvents =
+      await this.reservationService.getAllPaymentOutboxsByStatus(
+        PaymentOutboxStatus.INIT,
+      );
+
+    if (failedEvents.length === 0) return;
+
+    // 이벤트 재전송
+    for (const event of failedEvents) {
+      // 5분 이상 지난 이벤트만 재전송
+      if (event.createdAt.getTime() + 1000 * 60 * 5 <= new Date().getTime()) {
+        this.kafkaClient
+          .emit('payment', {
+            key: `payment_reservation_${event.metadata.reservationId}`,
+            value: {
+              eventId: event.id,
+              metadata: event.metadata,
+            },
+          })
+          .pipe(timeout(5000));
+      }
+    }
+  }
 }
